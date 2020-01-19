2a5f7fd21d276b7e9faae06a3ea57eeaf2f0645c
Your Name <akshat0123@gmail.com>
2020-01-19 01:12:16 -0500

    Initial commit

---
+++ public/styles-8544fb609db38bdd929b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{rMck:function(n,w,o){}}]);
+//# sourceMappingURL=styles-8544fb609db38bdd929b.js.map
---
+++ src/pages/index.js
@@ -0,0 +1,28 @@
+import Sidebar from "../components/sidebar";
+import Header from "../components/header";
+import Layout from "../components/layout";
+import Typist from "react-typist";
+import React from "react";
+
+export default () => (
+	<div>
+		<Header/>
+		<Layout>
+			<div>
+				<h1><Typist cursor={{hideWhenDone:true, hideWhenDoneDelay:250}}>About Me</Typist></h1>
+				<p>
+					I am a PhD student in computer science at George Washington
+					University. My advisor is Professor Aylin Caliskan and my
+					primary research interest is in bias and fairness in machine
+					learning.
+				</p>
+				<h1><Typist cursor={{hideWhenDone:true, hideWhenDoneDelay:250}}>Research</Typist></h1>
+				<p>
+					Bias and Fairness in AI, Natural Language Processing,
+					Machine Learning
+				</p>
+			</div>
+		</Layout>
+		<Sidebar/>
+	</div>
+)
